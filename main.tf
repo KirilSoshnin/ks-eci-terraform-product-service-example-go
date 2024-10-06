520734e9e74a6797868a1d923a696f01a0a8ad3d
Kiril <me@kirilsoshnin.com>
2024-10-06 16:28:01 -0400

    fix: adjust service name (#3)

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "service" {
   cluster_name = var.cluster_name
   environment  = var.environment
   image        = "${var.registry}/ks-eci-service-example-go:${var.environment}"
-  name         = "service"
+  name         = "service-example-go"
 
   parameters = [
     "ks-public-instructor",
