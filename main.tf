a110818a9665234aacdc2c376a4c11dabf4b0556
Kiril <me@kirilsoshnin.com>
2024-10-06 15:57:53 -0400

    feat: add resources (#1)

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "service" {
+  source = "./module/ecs-service"
+
+  cluster_name = var.cluster_name
+  environment  = var.environment
+  image        = "${var.registry}/ks-eci-service:${var.environment}"
+  name         = "service"
+
+  parameters = [
+    "ks-public-instructor",
+    "ks-public-location",
+    "ks-version"
+  ]
+}
